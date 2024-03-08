189c590e79ae3777c6d48d8cac3d9dd0935f2b0c
NeilMagon <neil.magon97@gmail.com>
2024-03-08 16:30:29 +0100

    feat: add game dice

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,3 +1,21 @@
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// Genero un numero per l'utente
+let user = Math.floor(Math.random() * 6) +1;
+console.log(user);
+
+// Genero un numero per il pc
+let pc = Math.floor(Math.random() * 6) +1;
+console.log(pc);
+
+// il numero più alto vince
+let winner;
+if (user > pc) {
+    winner = `l'utente ha vinto`;
+} else if (pc > user) {
+    winner = `il pc ha vinto`;
+}
+console.log(winner);
+
