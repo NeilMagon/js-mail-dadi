7f2ad8a4232d6533a29220ea627ce6cccf799634
NeilMagon <neil.magon97@gmail.com>
2024-03-08 16:58:44 +0100

    feat: add mail js

--- mail/js/script.js
+++ mail/js/script.js
@@ -3,3 +3,27 @@
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
 // Per la soluzione di questo esercizio NON è consentito utilizzare .includes di js!
+
+// Chiedo la mail all'utente
+const userMail = prompt(`Dimmi la tua email`);
+console.log(userMail);
+
+// Lista di email
+const mail = [`pino.mail.com`, `jonny.mail.com`, `arturo.mail.com`, `pippo.mail.com`];
+console.log(mail);
+
+// Confronto mail utente nella mia lista mail
+let mailFound = false;
+
+for (let i = 0; i < mail.length; i++) {
+    const mailPc = mail[i];
+    if (userMail === mailPc) {
+        mailFound = true;
+    }
+}
+if (mailFound === true) {
+    alert(`la tua mail è registrata`)
+} else {
+    alert(`la tua mail non è registrata`)
+}
+console.log(mailFound);
